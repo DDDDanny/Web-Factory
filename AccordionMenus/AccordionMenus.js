cb4c0fb3dd4f95df8dc2aeb3606d7101d8f45cb4
DDDDanny <707956456@qq.com>
2017-09-18 22:47:49 +0800

    新增代码注释

--- AccordionMenus/AccordionMenus.js
+++ AccordionMenus/AccordionMenus.js
@@ -4,15 +4,15 @@
 $(document).ready(function() {
   $("div>ul>li").on('click', function() {
     if ($(this).next('ul').is(':hidden')) {
-      //
+      //保证所有没有被点击的元素不旋转
       $("ul li").children('span:last-child').rotate({
         animateTo:0
       });
-      //
+      //使得被点击的元素旋转
       $(this).children('span:last-child').rotate({
         animateTo:180
       });
-      //
+      //保证其他选项卡颜色不变
       $("ul li").css('color', 'black');
       $(this).css('color', '#1cddb1');
     }else{
@@ -22,6 +22,7 @@ $(document).ready(function() {
       $(this).css('color', 'black');
     }
 
+    //控制最后一个选项卡的圆角形态
     if ($("#last").next('ul').is(':hidden')) {
       $(this).css('border-radius', '0px');
       $("div>ul>li:first-child").css('border-radius', '5px 5px 0px 0px');
@@ -29,11 +30,9 @@ $(document).ready(function() {
     else {
       $("#last").css('border-radius', '0px 0px 5px 5px');
     }
-    //
+    //保证打开的选项卡只有一个
     $(".two").slideUp(400);
-    //
     $(this).next('ul').stop();
-    //
     $(this).next('ul').slideToggle(400);
   });
 });
