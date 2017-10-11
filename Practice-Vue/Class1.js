e5bbcdaa80c6c4f743c80e395ee86fae82ca62e2
DDDDanny <707956456@qq.com>
2017-10-11 19:49:03 +0800

    Practice1

---
+++ Practice-Vue/Class1.js
@@ -0,0 +1,44 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+});
+
+var app2 = new Vue({
+  el: '#app2',
+  data: {
+    message2: '哇！出现了日期：'+ new Date().toLocaleString()
+  }
+});
+
+var app3 = new Vue({
+  el: '#app3',
+  data: {
+    seen: true
+  }
+});
+
+var app4 = new Vue({
+  el: '#app4',
+  data: {
+    list:[
+      {text: '哇！这样可以成功吗？'},
+      {text: '真的可以啊！'},
+      {text: '这样写很简单！'},
+      {text: 'Vue真的很棒棒！！！'}
+    ]
+  }
+});
+
+var app5 = new Vue({
+  el: '#app5',
+  data: {
+    message3: 'Hello Vue.js'
+  },
+  methods: {
+    reverseMessage :function(){
+      this.message3 = this.message3.split('').reverse().join('');
+    }
+  }
+});
